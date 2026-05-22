ec562803dbeba6116a3505b20c03c35b3ad8c951
g-dias-maciel <74046323+g-dias-maciel@users.noreply.github.com>
2026-05-22 16:04:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -20,6 +20,22 @@
         fbq('init', '752550821217294');
         fbq('track', 'PageView');
     </script>
+    <!-- Matomo -->
+    <script>
+      var _paq = window._paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//analytics.vaif.com.br/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <!-- End Matomo Code -->
+
     <noscript><img height="1" width="1" style="display:none"
         src="https://www.facebook.com/tr?id=752550821217294&ev=PageView&noscript=1"
     /></noscript>
